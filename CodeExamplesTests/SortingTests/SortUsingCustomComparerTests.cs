81f8d8bb1bf8914e07e55e242d52ee625f7eb72f
EngineerLondon <git@engineerlondon.co.uk>
2019-12-01 23:26:11 +0000

    fixing typo in test name

--- CodeExamplesTests/SortingTests/SortUsingCustomComparerTests.cs
+++ CodeExamplesTests/SortingTests/SortUsingCustomComparerTests.cs
@@ -6,7 +6,7 @@ namespace CodeExamplesTests.SortingTests
     public class SortUsingCustomComparerTests
     {
         [Test]
-        public void QuickSortSimplesStringArrayTest()
+        public void QuickSortSimpleStringArrayTest()
         {
             string[] expected = { "1", "2", "8", "100", "111", "200",
                 "3084193741082937", "3084193741082938", "12303479849857341718340192371" };
