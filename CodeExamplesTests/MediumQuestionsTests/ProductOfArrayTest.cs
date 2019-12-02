2b1171efae25da48fa3af2a1a75d384019043a2c
EngineerLondon <git@engineerlondon.co.uk>
2019-12-02 15:43:28 +0000

    Adding test for ProductOfArray without Divide question

--- CodeExamplesTests/MediumQuestionsTests/ProductOfArrayTest.cs
+++ CodeExamplesTests/MediumQuestionsTests/ProductOfArrayTest.cs
@@ -22,21 +22,21 @@ namespace CodeExamplesTests.MediumQuestionsTests
             Assert.AreEqual(expected, result);
         }
 
-        //[Test]
-        //public void CalculateProductWithoutDivideArrTest()
-        //{
-        //    var arr1 = new[] { 1, 2, 3, 4, 5 };
-        //    var expected = new[] { 120, 60, 40, 30, 24 };
-        //    /*
-        //    2 * 3 * 4 * 5 = 120
-        //    1 * 3 * 4 * 5 = 60
-        //    1 * 2 * 4 * 5 = 40
-        //    1 * 2 * 3 * 5 = 30
-        //    1 * 2 * 3 * 4 = 24
-        //    */
+        [Test]
+        public void CalculateProductWithoutDivideArrTest()
+        {
+            var arr1 = new[] { 1, 2, 3, 4, 5 };
+            var expected = new[] { 120, 60, 40, 30, 24 };
+            /*
+            2 * 3 * 4 * 5 = 120
+            1 * 3 * 4 * 5 = 60
+            1 * 2 * 4 * 5 = 40
+            1 * 2 * 3 * 5 = 30
+            1 * 2 * 3 * 4 = 24
+            */
 
-        //    var result = CodeExamples.MediumQuestions.CalculateProductArr.CalcProductsWithoutDivide(arr1);
-        //    Assert.AreEqual(expected, result);
-        //}
+            var result = CodeExamples.MediumQuestions.CalculateProductArr.CalcProductsWithoutDivide(arr1);
+            Assert.AreEqual(expected, result);
+        }
     }
 }
