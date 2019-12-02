dd799dd168e1ac7ba40f1ecb74d9a0e39e7d8161
EngineerLondon <git@engineerlondon.co.uk>
2019-12-02 15:42:23 +0000

    Adding simple solution for Product of Array Question.

--- CodeExamples/MediumQuestions/CalculateProductArr.cs
+++ CodeExamples/MediumQuestions/CalculateProductArr.cs
@@ -4,6 +4,40 @@
     {
         public static int[] CalcProducts(int[] arr)
         {
+            if (arr.Length < 1)
+            {
+                return arr;
+            }
+
+            int product = 1;
+            foreach (var value in arr)
+            {
+                product *= value;
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = product / arr[i];
+            }
+
+            return arr;
+        }
+
+        public static int[] CalcProductsWithoutDivide(int[] arr)
+        {
+            if (arr.Length < 1)
+            {
+                return arr;
+            }
+
+            /*
+                2 * 3 * 4 * 5 = 120
+                1 * 3 * 4 * 5 = 60
+                1 * 2 * 4 * 5 = 40
+                1 * 2 * 3 * 5 = 30
+                1 * 2 * 3 * 4 = 24
+            */
+
             var result = new int[arr.Length];
 
             return result;
--- CodeExamplesTests/MediumQuestionsTests/ProductOfArrayTest.cs
+++ CodeExamplesTests/MediumQuestionsTests/ProductOfArrayTest.cs
@@ -9,8 +9,34 @@ namespace CodeExamplesTests.MediumQuestionsTests
         {
             var arr1 = new[] { 1, 2, 3, 4, 5 };
             var expected = new[] { 120, 60, 40, 30, 24 };
+            /*
+            2 * 3 * 4 * 5 = 120
+            1 * 3 * 4 * 5 = 60
+            1 * 2 * 4 * 5 = 40
+            1 * 2 * 3 * 5 = 30
+            1 * 2 * 3 * 4 = 24
+            */
+            // Calculate the product of all then divide by the number in question O(n)
+
             var result = CodeExamples.MediumQuestions.CalculateProductArr.CalcProducts(arr1);
             Assert.AreEqual(expected, result);
         }
+
+        //[Test]
+        //public void CalculateProductWithoutDivideArrTest()
+        //{
+        //    var arr1 = new[] { 1, 2, 3, 4, 5 };
+        //    var expected = new[] { 120, 60, 40, 30, 24 };
+        //    /*
+        //    2 * 3 * 4 * 5 = 120
+        //    1 * 3 * 4 * 5 = 60
+        //    1 * 2 * 4 * 5 = 40
+        //    1 * 2 * 3 * 5 = 30
+        //    1 * 2 * 3 * 4 = 24
+        //    */
+
+        //    var result = CodeExamples.MediumQuestions.CalculateProductArr.CalcProductsWithoutDivide(arr1);
+        //    Assert.AreEqual(expected, result);
+        //}
     }
 }
