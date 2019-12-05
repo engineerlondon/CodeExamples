1c1a46dd234bd388256e368b42973b9cc6a06fa2
EngineerLondon <git@engineerlondon.co.uk>
2019-12-05 16:57:16 +0000

    Adding Day 4 part 1 Secure Container

---
+++ CodeExamples/AdventOfCode2019/Day4SecureContainer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodeExamples.AdventOfCode2019
+{
+    public class Day4SecureContainer
+    {
+        public int FindIntThatMatchesRules(int low, int high)
+        {
+            List<int> solutions = new List<int>();
+            int counter = 0;
+            /*
+               It is a six-digit number.
+               The value is within the range given in your puzzle input.
+               Two adjacent digits are the same (like 22 in 122345).
+               Going from left to right, the digits never decrease; they only ever increase or stay the same (like 111123 or 135679).
+
+             */
+
+            for (int i = low; i <= high; i++)
+            {
+                var numAsString = i.ToString();
+                if (numAsString.Length != 6)
+                    continue;
+                var previous = '@';
+                var twoAdjacent = false;
+
+                foreach (var elem in numAsString.ToCharArray())
+                {
+                    if (elem == previous)
+                    {
+                        twoAdjacent = true;
+                        break;
+                    }
+                    previous = elem;
+                }
+                if (!twoAdjacent)
+                    continue;
+
+                int previousCharVal = numAsString[5];
+                bool increasing = true;
+                for (int j = 4; j >= 0; j--)
+                {
+                    if (numAsString[j] > previousCharVal)
+                    {
+                        increasing = false;
+                    }
+
+                    previousCharVal = numAsString[j];
+                }
+
+                if (increasing)
+                {
+                    solutions.Add(i);
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+    }
+}
---
+++ CodeExamplesTests/AdventOfCode2019/Day4SecureContainerTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CodeExamples.AdventOfCode2019;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace CodeExamplesTests.AdventOfCode2019
+{
+    public class Day4SecureContainerTests
+    {
+        [Test]
+        public void FindIntThatMatchesRulesTestExample0()
+        {
+            Day4SecureContainer container = new Day4SecureContainer();
+            int result = container.FindIntThatMatchesRules(111111, 111111);
+            Assert.That(result, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void FindIntThatMatchesRulesTestExample1()
+        {
+            Day4SecureContainer container = new Day4SecureContainer();
+            int result = container.FindIntThatMatchesRules(223450, 223450);
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void FindIntThatMatchesRulesTestExample2()
+        {
+            Day4SecureContainer container = new Day4SecureContainer();
+            int result = container.FindIntThatMatchesRules(123789, 123789);
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void FindIntThatMatchesRulesTestExample3()
+        {
+            Day4SecureContainer container = new Day4SecureContainer();
+            int result = container.FindIntThatMatchesRules(135679, 135679);
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void FindIntThatMatchesRulesTestExample4()
+        {
+            Day4SecureContainer container = new Day4SecureContainer();
+            int result = container.FindIntThatMatchesRules(111123, 111123);
+            Assert.That(result, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void FindIntThatMatchesRuleActualDataTest()
+        {
+            Day4SecureContainer container = new Day4SecureContainer();
+            int result = container.FindIntThatMatchesRules(145852, 616942);
+            Assert.That(result, Is.EqualTo(1767));
+        }
+    }
+}
